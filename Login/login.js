d1874361facc5b33dc6f54c83c4469cf9ba4abec
Saad_Naseem <raosad786@gmail.com>
2025-05-11 04:59:06 +0500

    Add complete structure for Admin, Dashboard

---
+++ Login/login.js
@@ -0,0 +1,6 @@
+let formFeild = document.querySelectorAll("form input");
+
+const [loginEmail, loginPassword] = formFeild;
+
+let loginBtn = document.getElementById('loginBtn');
+let forgotPass = document.getElementById('forgot-password');
