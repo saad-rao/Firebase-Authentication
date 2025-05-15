7d1408e3db3bba3dd1de35991e51d93001f95ac6
Saad_Naseem <raosad786@gmail.com>
2025-05-15 16:16:37 +0500

    Add login functionality with Toast notifications for success and error messages.

--- Login/login.js
+++ Login/login.js
@@ -1,6 +1,47 @@
+import { auth, signInWithEmailAndPassword } from "../firebase.js";
+
+
 let formFeild = document.querySelectorAll("form input");
 
 const [loginEmail, loginPassword] = formFeild;
 
 let loginBtn = document.getElementById('loginBtn');
+
+
+
+
+
+const Login = ()=>{
+    event.preventDefault();
+    signInWithEmailAndPassword(auth, loginEmail.value ,loginPassword.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+  Toastify({
+
+text: "Login  Successfully",
+
+duration: 3000
+
+}).showToast();
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    Toastify({
+
+text:`${errorMessage}`,
+
+duration: 3000
+
+}).showToast();
+  });
+
+    
+}
+
+
+loginBtn.addEventListener("click",Login)
+
+
 let forgotPass = document.getElementById('forgot-password');
